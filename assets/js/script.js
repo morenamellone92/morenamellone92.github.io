ceb7a96417b644a388107babc2242055686f6567
Morena Mellone - Front End Developer <100976734+morenamellone92@users.noreply.github.com>
2022-04-20 04:03:56 +0200

    aggiornamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ function MenuMobile(){
 if(menu == "close"){
   mobile_menu.style.width="100%";
   menu='open';
-  iconmenu.src = "/../../assets/img/menu_close.svg";
+  iconmenu.src = "/../../assets/img/menu_close.png";
 }
 
 else
